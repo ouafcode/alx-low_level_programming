1dc36b7f1392f4e98ba535285aef4f5b6d3b7229
ouafcode <ouafaesaim@gmail.com>
2023-02-17 13:22:32 -0800

    except

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
