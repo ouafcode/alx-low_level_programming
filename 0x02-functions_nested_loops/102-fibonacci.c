15d94b4a9ae73324a806b6d445e9b56c58a82947
ouafcode <ouafaesaim@gmail.com>
2023-02-22 13:46:11 -0800

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	long  x1 = 1, x2 = 2, nextval;
+
+	printf("%ld, ", x1);
+	printf("%ld, ", x2);
+	for (i = 2; i < 50; i++)
+	{
+		nextval = x2;
+		x2 = x2 + x1;
+		x1 = nextval;
+		printf(", %ld", x2);
+	}
+	printf("\n");
+	return (0);
+}
