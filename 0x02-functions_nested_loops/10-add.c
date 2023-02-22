69d9240f0a585b1000194ed7588f337e4257c802
ouafcode <ouafaesaim@gmail.com>
2023-02-22 08:26:14 -0800

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
  * add - add two integers
- *
+ * @a: input integer value
+ * @b: input integer value
  * Return: n equal addition of two integers
  */
-int add(int a,int b)
+int add(int a, int b)
 {
 	int n;
 
