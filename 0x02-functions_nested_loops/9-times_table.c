f9bef393343ddf165af99386475b16c1173175f1
ouafcode <ouafaesaim@gmail.com>
2023-02-21 19:03:35 -0800

    time

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-		for (cl = 0; cl <= 9; cl++)
+		for (cl = 1; cl <= 9; cl++)
 		{
 			i = (rw * cl);
 			if ((i / 10) > 0)
