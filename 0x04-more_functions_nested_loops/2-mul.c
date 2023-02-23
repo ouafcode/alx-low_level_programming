093e15b52f438642618cc16f4ed67402519743bd
ouafcode <ouafaesaim@gmail.com>
2023-02-23 11:26:00 -0800

    task1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two integers
+ *
+ * @a: input integer
+ * @b: input integer
+ * Return: mul value of a and b
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 Task 0 : create function that checks for uppercase character
-Task 1 : create function that checks digit 
+Task 1 : create function that checks digit
+Task 2 : create functions that multiplies two integers 
