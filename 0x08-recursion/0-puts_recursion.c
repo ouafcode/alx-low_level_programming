a0aa96550ff7e9052e9c125145c8dc4bad16b6ca
ouafcode <ouafaesaim@gmail.com>
2023-03-08 10:23:48 -0800

    Task0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string
+ *
+ * @s: pointer point to string
+ * Return: string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+}
