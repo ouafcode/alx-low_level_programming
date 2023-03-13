a67a514085eebbd95aa71ef2d94a3c56269f641e
ouafcode <ouafaesaim@gmail.com>
2023-03-13 09:06:53 -0700

    Task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - adding argument
+ * @argc: count number of argument
+ * @argv: array for entery argument
+ * Return: Always 0 (Success)
+ */
+int __attribute__ ((unused)) main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,1 +1,2 @@
 Task0: Write a program that prints its name, followed by a new line.
+Task1: Write a program that prints the number of arguments passed into it.
