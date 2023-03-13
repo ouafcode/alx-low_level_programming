65f455abc4a4044a47a776e636f2a5213db052a7
ouafcode <ouafaesaim@gmail.com>
2023-03-13 11:05:37 -0700

    Task2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - adding argument
+ * @argc: count number of argument
+ * @argv: array for entery argument
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
