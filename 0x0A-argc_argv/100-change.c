06d773b160e32709777563c0a716bdb74af1a632
ouafcode <ouafaesaim@gmail.com>
2023-03-15 15:45:51 -0700

    Task5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,38 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-/**
- * coinconvert - function to calculate number of coins
- * @i: input integer
- * Return: the number of coins
- */
-int coinconvert(int i)
-{
-	int count = 0;
-
-	while (i != 0)
-	{
-		if (i % 10 == 9 || i % 10 == 7)
-			i -= 2;
-		else if (i % 25 == 0)
-			i -= 25;
-		else if (i % 10 == 0)
-                        i -= 10;
-		else if (i % 5 == 0)
-                        i -= 5;
-		else if (i % 2 == 0)
-		{
-			if (i % 10 == 6)
-                        	i -= 1;
-			else
-				i -= 2;
-		}
-		else
-			i -= 1;
-		count++;
-	}
-	return (count);
-}
 /**
  * main - take argument for coin count
  * @argc: count number of argument
@@ -41,23 +8,33 @@ int coinconvert(int i)
  */
 int main(int argc, char *argv[])
 {
-	int i, coin;
-
-	coin = 0;;
+	int n, i, res;
+	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	i = atoi(argv[1]);
 
-	if (i < 0)
-		printf("0\n");
-	else
+	n = atoi(argv[1]);
+	res = 0;
+
+	if (n < 0)
 	{
-		coin = coinconvert(i);
-		printf("%d\n", coin);
+		printf("0\n");
+		return (0);
 	}
+
+	for (i = 0; i < 5 && n >= 0; i++)
+	{
+		while (n >= coins[i])
+		{
+			res++;
+			n -= coins[i];
+		}
+	}
+
+	printf("%d\n", res);
 	return (0);
 }
