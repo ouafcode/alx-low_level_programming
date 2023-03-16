950fc757eb0b4f1bcf2d7b6545be0e3b6ed5ac6b
ouafcode <ouafaesaim@gmail.com>
2023-03-16 15:07:22 -0700

    Task2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,12 +14,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	ptr = malloc(nmemb * size);
-	if(ptr == NULL)
+	if (ptr == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	while (i < (nmemb * size))
 	{
