f8e83e03cb541581dea26d5fdc5201ab03d836ac
ouafcode <ouafaesaim@gmail.com>
2023-03-16 14:17:46 -0700

    Task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,41 +10,32 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j;
-	unsigned int x = 0;
-	unsigned int y = 0;
-	unsigned int k;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 	char *ptr;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		x++;
-	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		y++;
-	}
-	k = x + y;
-	if (n >= y)
-		n = y;
-	ptr = malloc(k);
-	if (ptr == NULL)
+	while (s1 && s1[len1])
+		len1++;
+
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		ptr = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		ptr = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!ptr)
 		return (NULL);
-	for (i = 0; i < x; i++)
+	while (i < len1)
 	{
 		ptr[i] = s1[i];
+		i++;
 	}
-	for (i = x; i < k; i++)
-	{
-		for (j = 0; j < n; j++)
-		{
-			ptr[i] = s2[j];
-			i++;
-		}
-	}
+	while (n < len2 && i < (len1 + n))
+		ptr[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		ptr[i++] = s2[j++];
+
+	ptr[i] = '\0';
 	return (ptr);
 }
