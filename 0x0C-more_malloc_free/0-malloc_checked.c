b9e791c11ac2d04d990e609d44ac938c70f1b008
ouafcode <ouafaesaim@gmail.com>
2023-03-16 12:21:09 -0700

    Task0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked - function that allocate memory
+ * @b: input size
+ * Return: if function fail , should cause normal process
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	if (b == INT_MAX)
+	{
+		exit(98);
+	}
+	p = malloc(b);
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+Task0: Write a function that allocates memory using malloc.if malloc fails, the malloc_checked function should cause normal process termination with a status value of 98
