5a30e28b359bbcbc1d85c6e9ef23b7af1ff7531c
ouafcode <ouafaesaim@gmail.com>
2023-03-21 13:09:50 -0700

    Task0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+Task0: Define a new type struct dog with the following elements name,age,owner
+Task1: 
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - define new type of dogs
+ * @name: first member name of dog
+ * @age: second member age of dog
+ * @owner: third member
+ *
+ * Description: Long description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
