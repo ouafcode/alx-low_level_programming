36bd1f36eeb6beebdf49ce14e5ed49e1ddeee846
ouafcode <ouafaesaim@gmail.com>
2023-03-23 14:50:08 -0700

    Task3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_duv(int a, int b);
+
+/**
+ * op_add - Returns the sums of two numbers.
+ * @a: the first number
+ * @b: the second numbers
+ *
+ * Return: the sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_add - Returns the difference of two numbers.
+ * @a: the first number
+ * @b: the second numbers
+ *
+ * Return: the difference of a and b
+ */
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+/**
+ * op_mul - Returns the multiplication of two numbers.
+ * @a: the first number
+ * @b: the second numbers
+ *
+ * Return: the multi of a and b
+ */
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+/**
+ * op_div - Returns the division of two numbers.
+ * @a: the first number
+ * @b: the second numbers
+ *
+ * Return: the division of a and b
+ */
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+/**
+ * op_mod - Returns the reminder  of two numbers.
+ * @a: the first number
+ * @b: the second numbers
+ *
+ * Return: the remainder  of division of  a by b
+ */
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,3 +1,4 @@
 Task0: Write a function that prints a name.
 Task1: Write a function that executes a function given as a parameter on each element of an array.
 Task2: Write a function that searches for an integer.
+Task3: Write a program that performs simple operations.
