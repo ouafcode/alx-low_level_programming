c6a1ae894d32785aa5d745430452b4931d7edef0
ouafcode <ouafaesaim@gmail.com>
2023-03-24 14:52:54 -0700

    Task2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		str = va_arg(arg, char *);
 
 		if (str == NULL)
-			printf("(nul)");
+			printf("(nil)");
 		else
 			printf("%s", str);
 
