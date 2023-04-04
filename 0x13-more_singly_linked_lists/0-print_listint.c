244ce4cc17dac5828ec3a9ff88fdd38aa663a5ea
ouafaesaim <ouafaesaim@gmail.com>
2023-04-04 03:34:38 -0700

    Task0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,12 +10,11 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t x = 0;
-	const listint_t *temp = h;
 
-	while (temp != NULL)
+	while (h != NULL)
 	{
-		printf("%d \n", temp->n);
-		temp = temp->next;
+		printf("%d \n", h->n);
+		h = h->next;
 		x++;
 	}
 	return (x);
