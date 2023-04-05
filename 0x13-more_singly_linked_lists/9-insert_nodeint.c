1164cf5118fa5495b160c3016f9a506ba7347ce8
ouafaesaim <ouafaesaim@gmail.com>
2023-04-04 17:14:57 -0700

    Task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			ptr->next = temp->next;
 			temp->next = ptr;
-			return (temp);
+			return (ptr);
 		}
 		else
 			temp = temp->next;
