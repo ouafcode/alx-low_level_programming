91afa26a2adcfe5ecb148061f32783a9c823fdc6
ouafaesaim <ouafaesaim@gmail.com>
2023-04-08 07:49:00 -0700

    Task4

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,6 +6,7 @@ unsigned int _pow_recursion(unsigned int x, unsigned int y);
 void print_binary(unsigned long int n);
 int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 
 #endif
