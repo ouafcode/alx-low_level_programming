390686ce2bdf3cb0a7a0b2a204b6d7256612af6b
ouafcode <ouafaesaim@gmail.com>
2023-08-17 14:19:11 -0700

    task5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - return  the nth node of a dlistint_t linked list
+ * @head: ptr to list
+ * @index: index of node
+ *
+ * Return: return address of node
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *p;
+	unsigned int i;
+
+	p = head;
+	i = 0;
+	if (head == NULL)
+		return(NULL);
+	while (p != NULL)
+	{
+		if (i == index)
+			return (p);
+		p = p->next;
+		i++;
+	}
+	return (NULL);
+}
