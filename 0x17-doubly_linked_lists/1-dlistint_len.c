7e9c01086adedf925063afdd6bd34738c1dcad5a
ouafcode <ouafaesaim@gmail.com>
2023-08-17 13:35:59 -0700

    task1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,10 +2,11 @@
 
 /**
  * dlistint_len - returns the number of elements in dlistint_t list
- * @h : head
+ * @h: head
  *
- * Return : nmr of element in list
+ * Return: nmr of element in list
  */
+
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t i = 0;
