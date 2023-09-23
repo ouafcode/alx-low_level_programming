9e3186d36188845f365f8b56b18c23ca0835f8b0
ouafcode <ouafaesaim@gmail.com>
2023-09-23 10:48:25 -0700

    task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,4 @@
-#include "hash_table.h"
+#include "hash_tables.h"
 /**
  * hash_table_set - adds an element to the hash table
  * @ht: hash table you want to add or update the key/value
@@ -12,37 +12,39 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *item;
 	unsigned long int index, i;
-	char val_copy;
+	char *val_copy;
 
-	if (ht == NULL or key == NULL or key == '\0' or index == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 	val_copy = strdup(value);
 	if (val_copy == NULL)
 		return (0);
 
-	index = key_index((const char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
-			free(ht->array[i]->value)
+		{
+			free(ht->array[i]->value);
 			ht->array[i]->value = val_copy;
 			return (1);
+		}
 	}
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
+	item = malloc(sizeof(hash_node_t));
+	if (item == NULL)
 	{
 		free(val_copy);
 		return (0);
 	}
-	new->key = strdup(key);
-	if (new->key == NULL)
+	item->key = strdup(key);
+	if (item->key == NULL)
 	{
-		free(new);
+		free(item);
 		return (0);
 	}
-	new->value = val_copy;
-	new->next = ht->array[index];
-	ht->array[index] = new;
+	item->value = val_copy;
+	item->next = ht->array[index];
+	ht->array[index] = item;
 
 	return (1);
 }
