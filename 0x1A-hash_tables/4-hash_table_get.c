12b7f7b0ccbeaa20e17f48a145b9ed16ff2ed95b
ouafcode <ouafaesaim@gmail.com>
2023-09-23 14:11:47 -0700

    task4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,7 +16,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((const unsigned char *)key, ht->size);
 	head = ht->array[index];
 
-	if (head != NULL)
+	while (head)
 	{
 		if (strcmp(head->key, key) == 0)
 		{
