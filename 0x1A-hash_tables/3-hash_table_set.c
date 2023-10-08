41186ca292f6e2434c154c6b289838e96bf5eb23
ouafcode <ouafaesaim@gmail.com>
2023-10-08 09:58:49 -0700

    task3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,54 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Add or update element
+ * @ht: pointer to the hash table.
+ * @key: The key to add
+ * @value: The value associated with key.
+ *
+ * Return: 0 if fails
+ * 
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	char *value_cp;
+	unsigned long int index, i;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_cp = strdup(value);
+	if (value_cp == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_cp;
+			return (1);
+		}
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_cp);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_cp;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
+	return (1);
+}
